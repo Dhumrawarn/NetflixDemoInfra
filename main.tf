5c4305f0509eea7bd36912590d15b95fbb2fad09
Dhumrawarn <93244919+Dhumrawarn@users.noreply.github.com>
2025-08-24 12:30:44 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+count = 4
+ami = "ami-00ca32bbc84273381"
+instance_type = "t2.medium"
+key_name = "argocd"
+vpc_security_group_ids = ["sg-0fb241bd6ca97819c"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+
+variable "instance_names" {
+default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
+}
